93a8646f4c24a7749cb8f20cdf9d4c211e1a2079
Tankhoa15 <tankhoa1503kh@gmail.com>
2024-04-13 12:51:47 +0700

    update code 13/04/2024

--- src/main/java/com/khoa/shop/controller/HomeController.java
+++ src/main/java/com/khoa/shop/controller/HomeController.java
@@ -1,37 +1,29 @@
 package com.khoa.shop.controller;
 
+import com.khoa.shop.entity.ProductEntity;
 import com.khoa.shop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
+@RequestMapping("/products")
 public class HomeController {
 
     @Autowired
-    ProductService product;
+    ProductService service;
 
-    @GetMapping("/home")
-    public String home() {
-        return "index";
+    //all product
+    private String getAllProduct(Model model){
+        List<ProductEntity> product = service.getAllProduct();
+        return "";
     }
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
 
-    @GetMapping("/register")
-    public String register() {
-        return "register";
-    }
-
-    // add product
-    @GetMapping("/add-product")
-    public String addProduct() {
-        return "addProduct";
-    }
 
 }
--- src/main/java/com/khoa/shop/service/ProductService.java
+++ src/main/java/com/khoa/shop/service/ProductService.java
@@ -11,27 +11,11 @@ import java.util.*;
 public class ProductService {
 
    @Autowired
-   ProductRepository productRepository;
+   ProductRepository repo;
 
-    public List<ProductEntity> findAll() {
-         return productRepository.findAll();
-    }
-
-    //create product
-    public ProductEntity createProduct(ProductEntity product) {
-        return productRepository.save(product);
-    }
-
-    // update product
-    public ProductEntity updateProduct(ProductEntity product) throws Exception {
-        return productRepository.findById(product.getId())
-                .map(existingProduct -> productRepository.save(product))
-                .orElseThrow(() -> new Exception("Product not found with id: " + product.getId()));
-    }
-
-    //delete product
-    public void deleteProduct(Long id) {
-        productRepository.deleteById(id);
+    public List<ProductEntity> getAllProduct() {
+        List<ProductEntity> products = (List<ProductEntity>) repo.findAll();
+        return products;
     }
 
 }
